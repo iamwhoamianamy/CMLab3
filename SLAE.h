b986884143336bbdfcc4f7e193b7353a865f6618
zadolbali10 <anon.yu@mail.ru>
2020-11-21 02:30:56 +0700

    Изменена структура матрицы. добавлена функция умножения матрицы на вектор

--- SLAE.h
+++ SLAE.h
@@ -9,14 +9,22 @@ class SLAE
 {
 public:
    int N;            // Размер матрицы
-   int M;            // Количество ненулевых элементов в треугольнике
+
+   int Ml;           // Количество ненулевых элементов в нижнем треугольнике
+   int Mu;           // Количество ненулевых элементов в верхнего треугольнике
+  
    int maxiter;      // Максимальное количество итераций
    real eps;         // Велечина требуемой относительной невязки
 
-   vector<int> ig;   // Указатели начала строк
-   vector<int> jg;   // Номера столбцов внедиагональных элементов
+   vector<int> igl;  // Указатели начала строк нижнего треугольника
+   vector<int> igu;  // Указатели начала строк верхнего треугольника
+
+   vector<int> jgl;  // Номера столбцов внедиагональных элементов нижнего треуголника
+   vector<int> jgu;  // Номера столбцов внедиагональных элементов верхнего треуголника
+   
    vector<real> ggl; // Верхний треугольник
    vector<real> ggu; // Нижний треугольник
+   
    vector<real> di;  // Диагональ
    vector<real> pr;  // Вектор правой части
    
@@ -29,11 +37,17 @@ public:
       fin >> N >> maxiter >> eps;
       fin.close();
 
-      read_vector<int>(path + "ig.txt", ig, N);
-      M = ig[N];
-      read_vector<int>(path + "jg.txt", jg, M);
-      read_vector<real>(path + "ggl.txt", ggl, M);
-      read_vector<real>(path + "ggu.txt", ggu, M);
+      read_vector<int>(path + "igl.txt", igl, N + 1);
+      Ml = igl[N];
+      read_vector<int>(path + "igu.txt", igu, N + 1);
+      Mu = igu[N];
+
+      read_vector<int>(path + "jgl.txt", jgl, Ml);
+      read_vector<int>(path + "jgu.txt", jgu, Mu);
+
+      read_vector<real>(path + "ggl.txt", ggl, Ml);
+      read_vector<real>(path + "ggu.txt", ggu, Mu);
+
       read_vector<real>(path + "di.txt", di, N);
       read_vector<real>(path + "pr.txt", pr, N);
    }
@@ -54,11 +68,28 @@ public:
    // Функция умножения матрицы на вектор vec, результат в res
    void matrix_vector_mult(const vector<real>& vec, vector<real>& res)
    {
-      res.resize(N);
-
       for (int i = 0; i < N; i++)
       {
+         // Диагональ
+         res[i] += vec[i] * di[i];
 
+         // Нижний треугольник
+         int prof_len = igl[i + 1] - igl[i];
+         for (int k = 0; k < prof_len; k++)
+         {
+            int index_in_prof = igl[i] + k;
+            int j = jgl[index_in_prof];
+            res[i] += vec[j] * ggl[index_in_prof];
+         }
+
+         // Верхний треугольник
+         prof_len = igu[i + 1] - igu[i];
+         for (int k = 0; k < prof_len; k++)
+         {
+            int i_in_prof = igu[i] + k;
+            int j = jgu[i_in_prof];
+            res[i] += vec[j] * ggu[i_in_prof];
+         }
       }
    }
 };
--- main.cpp
+++ main.cpp
@@ -6,4 +6,8 @@ using namespace std;
 int main()
 {
    SLAE slae = SLAE("test1/");
+
+   vector<real> res(4);
+
+   slae.matrix_vector_mult(slae.pr, res);
 }
